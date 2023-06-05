5530512d3c636a3e57a5f0829e212327a57f260f
lwzzzz1020000 <131642672+lwzzzz1020000@users.noreply.github.com>
2023-06-05 13:49:52 +0800

    Add files via upload

---
+++ 111/README.md
@@ -0,0 +1,4 @@
+# 希望可以暴富
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lwz1020/pen/BaOZXXb](https://codepen.io/lwz1020/pen/BaOZXXb).
+
---
+++ 111/dist/script.js
@@ -0,0 +1,8 @@
+document.write("我叫梁菀芝 夢想是暴富")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2
+      counterElement.textContent = count;
+    }
